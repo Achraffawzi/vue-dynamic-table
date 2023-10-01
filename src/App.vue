a2c200d0f2bdc5ca5f3a225bb62b97cef7e29d55
achraffawzi <achraffawzi224a@gmail.com>
2023-10-01 11:02:10 +0100

    feat: Implement select one or more items functionality

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,78 @@
 <template>
-  <div></div>
+  <div>
+    <DynamicTable
+      :headers="headers"
+      :data="data"
+      :pagination="pagination"
+      :is-selectable="isSelectable"
+    />
+  </div>
 </template>
 
-<script setup></script>
+<script setup>
+import DynamicTable from "@/components/DynamicTable.vue";
+import { ref } from "vue";
 
-<style></style>
+const headers = ref([
+  {
+    text: "Id",
+    sortable: false,
+  },
+  {
+    text: "Name",
+    sortable: true,
+  },
+  {
+    text: "Email",
+    sortable: false,
+  },
+  {
+    text: "Phone",
+    sortable: false,
+  },
+  {
+    text: "Company",
+    sortable: true,
+  },
+]);
+
+const data = ref([
+  {
+    id: 1,
+    name: "Leanne Graham",
+    email: "Lean@gmail.com",
+    phone: "1-770-736-8031 x56442",
+    company: "Romaguera-Crona",
+  },
+  {
+    id: 2,
+    name: "Ervin Howell",
+    email: "Ervin@gmail.com",
+    phone: "010-692-6593 x09125",
+    company: "Deckow-Crist",
+  },
+  {
+    id: 3,
+    name: "Clementine Bauch",
+    email: "clementine@gmail.com",
+    phone: "1-463-123-4447",
+    company: "Romaguera-Jacobson",
+  },
+]);
+
+const pagination = ref({
+  page: 1,
+  limit: 2,
+  totalPages: 2,
+});
+
+const isSelectable = ref(true);
+</script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+</style>
---
+++ src/components/DynamicTable.vue
@@ -0,0 +1,189 @@
+<template>
+  <div>
+    <table>
+      <thead>
+        <tr>
+          <th v-if="isSelectable">
+            <input
+              type="checkbox"
+              name="selectAll"
+              id="selectAll"
+              :checked="areAllSelected"
+              @change="setSelectedItems(null)"
+            />
+          </th>
+          <th v-for="{ text } in headers" :key="text">
+            {{ text }}
+          </th>
+          <th>Actions</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="item in data" :key="item.id">
+          <td v-if="isSelectable">
+            <input
+              type="checkbox"
+              :name="`selected-name-${item.id}`"
+              :id="`selected-id-${item.id}`"
+              @change="setSelectedItems(item.id)"
+              :checked="isChecked(item.id)"
+            />
+          </td>
+          <td v-for="{ text } in headers" :key="text">
+            {{ item[text.toLowerCase()] }}
+          </td>
+          <td class="action-dots">
+            <span @click="setOpenedAction(item.id)">...</span>
+            <!-- menu -->
+            <ul v-if="openedAction === item.id">
+              <li>
+                <!-- <font-awesome-icon :icon="['fas', 'pen']" /> -->
+                <span>Edit</span>
+              </li>
+              <li>
+                <!-- <font-awesome-icon :icon="['fas', 'pen']" /> -->
+                <span>Delete</span>
+              </li>
+            </ul>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script setup>
+import { ref, defineProps, computed } from "vue";
+
+const props = defineProps({
+  headers: {
+    type: Array,
+    required: true,
+  },
+  data: {
+    type: Array,
+    required: true,
+  },
+  pagination: {
+    type: Object,
+    required: true,
+  },
+  isSelectable: {
+    type: Boolean,
+    required: false,
+    default: false,
+  },
+});
+
+const selectedItems = ref([]);
+const areAllSelected = ref(false);
+const openedAction = ref(null);
+
+const isChecked = (id) => {
+  return selectedItems.value.includes(id);
+};
+
+const setSelectedItems = (id) => {
+  if (id === null) {
+    areAllSelected.value = !areAllSelected.value;
+    selectedItems.value = areAllSelected.value
+      ? props.data.map((item) => item.id)
+      : [];
+  } else {
+    if (areAllSelected.value) {
+      areAllSelected.value = false;
+    }
+    selectedItems.value = selectedItems.value.includes(id)
+      ? selectedItems.value.filter((item) => item !== id)
+      : [...selectedItems.value, id];
+  }
+};
+
+const setOpenedAction = (id) => {
+  openedAction.value = openedAction.value === id ? null : id;
+};
+</script>
+
+<style scoped>
+table {
+  width: 100%;
+  margin-bottom: 1rem;
+  background-color: #fff;
+  border-collapse: collapse;
+  text-align: center;
+}
+
+table th,
+table td {
+  padding: 0.75rem;
+  vertical-align: top;
+  border-top: 1px solid #dee2e6;
+}
+
+table thead th {
+  vertical-align: bottom;
+  border-bottom: 2px solid #dee2e6;
+  background-color: #f8f9fa;
+}
+
+tbody tr:hover {
+  background-color: #f8f9fa;
+}
+
+.action-dots {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: relative;
+}
+
+.action-dots > span {
+  border-radius: 50%;
+  width: 40px;
+  height: 40px;
+  cursor: pointer;
+  display: flex;
+  justify-content: center;
+  line-height: 27px;
+  font-size: 26px;
+  transition: all 0.15s linear;
+}
+
+.action-dots > span:hover {
+  background-color: #ececec;
+}
+
+.action-dots > span:active {
+  background-color: #c2c0c0;
+}
+
+.action-dots > ul {
+  position: absolute;
+  top: 25px;
+  right: 65%;
+  width: 200px;
+  background-color: #fff;
+  border: 1px solid #ececec;
+  border-radius: 5px;
+  padding: 0px;
+  margin: 0;
+  list-style: none;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+  z-index: 1;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+}
+
+.action-dots > ul > li {
+  width: 100%;
+  margin: 0;
+  padding: 15px;
+  text-align: left;
+  cursor: pointer;
+}
+
+.action-dots > ul > li:hover {
+  background-color: #ececec;
+}
+</style>
